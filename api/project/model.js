b90451163694e8d5145584f709006e9038d17749
Melanie Mendoza <melanie.mendoza@gmail.com>
2021-06-26 18:35:27 -0400

    projects model & router done

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,24 @@
 // build your `Project` model here
+const environment = require("../../data/dbConfig")
+
+function getProjects() {
+    return environment("projects").select("id", "project_name", "project_description", "project_completed")
+}
+
+async function add(project) {
+    const [id] = await environment("projects").insert(project)
+    return getById(id)
+}
+
+function getById(id) {
+    return environment("projects")
+        .select("id", "project_name", "project_description", "project_completed")
+        .where({ id })
+        .first()
+    }
+
+
+module.exports = {
+    getProjects,
+    add,
+}
